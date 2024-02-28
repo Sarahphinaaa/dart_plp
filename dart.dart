0dd87224166969d68843a3a45691e66684e4bb19
Sarahphinaaa <oyesanyasarah94@gmail.com>
2024-02-28 06:07:19 -0800

    Add files via upload

---
+++ dart.dart
@@ -0,0 +1,21 @@
+void main() {
+  // Integer (int)
+  int age = 30; // Stores whole numbers
+  print("Your age is: $age years old");
+
+  // Double (double)
+  double pi = 3.14159; // Stores decimal numbers
+  print("The value of pi is: $pi");
+
+  // String (String)
+  String name = "Alice"; // Stores text
+  print("Your name is: $name");
+
+  // List (List)
+  List<String> fruits = ["apple", "banana", "orange"]; // Stores ordered collections of elements
+  print("List of fruits: $fruits");
+
+  // Map (Map)
+  Map<String, int> capitals = {"USA": 328200, "India": 1411718820}; // Stores key-value pairs
+  print("Population of capitals: $capitals");
+}
